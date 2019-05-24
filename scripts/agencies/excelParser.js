affabfe52ff40900e970828b0ef8887b31663d09
foreverpw <pwpwforever@gmail.com>
2019-05-25 00:43:28 +0800

    f

--- scripts/agencies/excelParser.js
+++ scripts/agencies/excelParser.js
@@ -3,7 +3,15 @@ var xlsx = require('node-xlsx');
 const workSheetsFromFile = xlsx.parse(`${__dirname}/agencies.xlsx`);
 let list = workSheetsFromFile[0].data
 let result = {}
-for (let i = i; i < list.length; i++) {
+for (let i = 1; i < list.length; i++) {
   const row = list[i];
-  result
+  result[row[4]] = result[row[4]] || []
+  result[row[4]].push({
+    shnumber:row[0],
+    shortName:row[1],
+    name:row[2],
+    province:row[3],
+    city:row[4]
+  })
 }
+console.log(JSON.stringify(result))
