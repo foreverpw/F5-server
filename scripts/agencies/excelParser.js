b3088361dbe291ca803c4acb95396e201dcb4575
mac <mac@macdeMacBook-Pro-2.local>
2019-05-27 14:08:20 +0800

    f

--- scripts/agencies/excelParser.js
+++ scripts/agencies/excelParser.js
@@ -3,9 +3,14 @@ var xlsx = require('node-xlsx');
 const workSheetsFromFile = xlsx.parse(`${__dirname}/agencies.xlsx`);
 let list = workSheetsFromFile[0].data
 let result = {}
+let locations = {}
 for (let i = 1; i < list.length; i++) {
   const row = list[i];
   result[row[4]] = result[row[4]] || []
+  locations[row[3]] = locations[row[3]] || []
+  if(locations[row[3]].indexOf(row[4])===-1){
+    locations[row[3]].push(row[4])
+  }
   result[row[4]].push({
     shnumber:row[0],
     shortName:row[1],
@@ -14,4 +19,4 @@ for (let i = 1; i < list.length; i++) {
     city:row[4]
   })
 }
-console.log(JSON.stringify(result))
+console.log(JSON.stringify(locations))
